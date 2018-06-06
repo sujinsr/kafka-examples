765d8c65a9de27682c4f7fd7ef595d6c420bf1b2
sujinsr <sujinsr@gmail.com>
2018-06-06 12:43:42 +0530

    Changed depreated classes and added number validation

--- kafka-streams-processor-api/src/main/java/org/kafka/streams/processor/api/NumberStreamTransform.java
+++ kafka-streams-processor-api/src/main/java/org/kafka/streams/processor/api/NumberStreamTransform.java
@@ -6,34 +6,34 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.processor.TopologyBuilder;
+import org.apache.kafka.streams.Topology;
 
 public class NumberStreamTransform {
 
 	public static void main(String []args) {
-        //System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "INFOl");
+		//System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "INFOl");
 
-		TopologyBuilder builder = new TopologyBuilder();
 		StringSerializer stringSerializer = new StringSerializer();
 		StringDeserializer stringDeserializer = new StringDeserializer();
-		
-		builder.addSource("SOURCE", stringDeserializer, stringDeserializer, "in-number")
-			.addProcessor("PROCESS-VALIDATE", NumberValidateProcessor::new, "SOURCE")
-			.addProcessor("PROCESS-DOUBLE", NumberDoubleProcessor::new, "PROCESS-VALIDATE")
-			.addProcessor("PROCESS-TRIPLE", NumberTripleProcessor::new, "PROCESS-VALIDATE")
-			.addSink("SINK-DOUBLE", "out-double-number", stringSerializer, stringSerializer, "PROCESS-DOUBLE")
-			.addSink("SINK-TRIPLE", "out-triple-number", stringSerializer, stringSerializer, "PROCESS-TRIPLE");
-		
+
+		Topology topology = new Topology();
+		topology.addSource("SOURCE", stringDeserializer, stringDeserializer, "in-number")
+		.addProcessor("PROCESS-VALIDATE", NumberValidateProcessor::new, "SOURCE")
+		.addProcessor("PROCESS-DOUBLE", NumberDoubleProcessor::new, "PROCESS-VALIDATE")
+		.addProcessor("PROCESS-TRIPLE", NumberTripleProcessor::new, "PROCESS-VALIDATE")
+		.addSink("SINK-DOUBLE", "out-double-number", stringSerializer, stringSerializer, "PROCESS-DOUBLE")
+		.addSink("SINK-TRIPLE", "out-triple-number", stringSerializer, stringSerializer, "PROCESS-TRIPLE");
+
 		StreamsConfig config = new StreamsConfig(getProperties());
-		KafkaStreams stream = new KafkaStreams(builder, config);
+		KafkaStreams stream = new KafkaStreams(topology, config);
 		stream.start();
 	}
-	
-    private static Properties getProperties() {
-        Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "testing-streams-processor-api");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 1);
-        return props;
-    }
+
+	private static Properties getProperties() {
+		Properties props = new Properties();
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "testing-streams-processor-api");
+		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 1);
+		return props;
+	}
 }
--- kafka-streams-processor-api/src/main/java/org/kafka/streams/processor/api/NumberValidateProcessor.java
+++ kafka-streams-processor-api/src/main/java/org/kafka/streams/processor/api/NumberValidateProcessor.java
@@ -1,5 +1,6 @@
 package org.kafka.streams.processor.api;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.kafka.streams.processor.AbstractProcessor;
 
 
@@ -8,7 +9,12 @@ public class NumberValidateProcessor extends AbstractProcessor<String, String> {
 	@Override
 	public void process(String key, String value) {
 		System.out.println("Number validation processor:: validating the value=" + value);
-		context().forward(key, value);
+		
+		if (NumberUtils.isCreatable(value)) {
+			context().forward(key, value);
+		} else {
+			System.out.println("Number validation processor:: Invalid number=" + value);
+		}
 		context().commit();
 	}
 
